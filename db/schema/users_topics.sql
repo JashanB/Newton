c29d8ddc5b1e4b57b7c4fd53f6d30680dba203ea
JashanB <53019207+JashanB@users.noreply.github.com>
2020-01-10 18:05:31 -0500

    Update users_topics.sql

--- db/schema/users_topics.sql
+++ db/schema/users_topics.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_topics CASCADE;
 
-CREATE TABLE users_topics(
-   user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+CREATE TABLE user_topics(
+   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
    topic_id INTEGER REFERENCES topics(id) ON DELETE CASCADE,
    PRIMARY KEY (user_id, resource_id);
