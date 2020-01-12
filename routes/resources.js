11e1dec1c4fb6923a1f60df988ed939dc8d093ff
Jamie <jamiekaram16@gmail.com>
2020-01-12 19:28:34 +0000

    Route for /resources, Display users from database

--- routes/resources.js
+++ routes/resources.js
@@ -2,15 +2,11 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-
-
   router.get("/", (req, res) => {
       db.query(`SELECT * FROM users;`)
         .then(data => {
-          const users = data.rows;
-          res.render('../views/resources', {users})
-          console.log(users)
-
+          const users = data.rows
+          res.render('../views/resources', { users })
         })
         .catch(err => {
           res
