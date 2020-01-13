f2896e5603f53d8f7c0939db65f097353151f337
Jamie <jamiekaram16@gmail.com>
2020-01-13 19:51:31 +0000

    Uses database.js

--- database.js
+++ database.js
@@ -3,13 +3,26 @@ const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
-const getResources = function() {
+const getResources = function () {
   return db.query(`SELECT * FROM resources`)
-  .then(data => {
-    return data.rows;
-  });
+    .then(data => {
+      return data.rows;
+    });
+}
+
+const resourceInfo = (id) => {
+  return db.query(`SELECT resources.*, comments.*, count(likes.id) as likes FROM resources
+                  LEFT JOIN likes ON likes.resource_id = resources.id
+                  LEFT JOIN comments ON comments.resource_id = resources.id
+                  WHERE resources.id = $1
+                  GROUP BY resources.id, comments.id;`, [id])
+    .then(data => {
+      return data.rows[0];
+    });
+
 }
 exports.getResources = getResources;
+exports.resourceInfo = resourceInfo;
 
 
 
--- routes/resources.js
+++ routes/resources.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = (db) => {
 
+module.exports = (db) => {
 
   router.get("/:id", (req, res) => {
     console.log(req.params.id)
-    db.query(`SELECT resources.*, comments.*, count(likes.id) as likes FROM resources
-              LEFT JOIN likes ON likes.resource_id = resources.id
-              LEFT JOIN comments ON comments.resource_id = resources.id
-              WHERE resources.id = $1
-              GROUP BY resources.id, comments.id;`, [req.params.id])
+    db.resourceInfo(req.params.id)
       .then(data => {
-        console.log('DATATATATA',data.rows[0])
-        const resource = data.rows[0]
+        const resource = data
+        console.log('DATA', data)
         res.render('../views/resources', { resource })
       })
       .catch(err => {
