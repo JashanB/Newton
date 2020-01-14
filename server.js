88fa27e1ba24661d36436aa3b3ff751c14477ae5
Jamie <jamiekaram16@gmail.com>
2020-01-14 17:20:46 +0000

    adding some styling files

--- server.js
+++ server.js
@@ -51,6 +51,9 @@ const homepageRoutes = require("./routes/homepage")
 const loginRoutes = require("./routes/login");
 const profileRoutes = require("./routes/profile");
 
+// CSS Links
+app.use("/styles",express.static(__dirname + "/styles"));
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/resources", resourceRoutes(database));
