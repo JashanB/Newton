bd1c48c9be7ec8f16e5a405f449afef978f509ca
Jashan <jashan_brar50@hotmail.com>
2020-01-14 18:19:40 +0000

    add profile check for homepage

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,63 +1,74 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
-router.get("/", (req, res) => {
-  db.getResourcesOrderByCountRating()
-  .then(data => {
-    const resource = {data:data};
-    res.render('index', { resource });
-  })
-  .catch(err => {
-    console.error(err);
+  router.get("/", (req, res) => {
+    db.getResourcesOrderByCountRating()
+      .then(data => {
+        const resource = { data: data };
+        res.render('index', { resource });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    // res.render('index')
   });
-  // res.render('index')
-});
 
-router.post("/search", (req, res) => {
-  const topicName = req.body.search;
-  console.log(req.body)
-  db.getResourcesByTopicName(topicName)
-  .then(data => {
-    // const resource = {data:data};
-    // res.render('index', { resource });
-  })
-  .catch(err => {
-    console.error(err);
+  router.post("/search", (req, res) => {
+    const topicName = req.body.search;
+    console.log(req.body)
+    db.getResourcesByTopicName(topicName)
+      .then(data => {
+        // const resource = {data:data};
+        // res.render('index', { resource });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   });
-});
 
-// router.get("/mostrecent", (req, res) => {
-//   db.getResourcesByCreatedAt()
-//   .then(data => {
-//     const resource = {data:data};
-//     res.render('mostrecent', { resource });
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-// });
+  // router.get("/mostrecent", (req, res) => {
+  //   db.getResourcesByCreatedAt()
+  //   .then(data => {
+  //     const resource = {data:data};
+  //     res.render('mostrecent', { resource });
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //   });
+  // });
 
-router.get("/signup", (req,res) => {
-  res.render("signup");
-})
-
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  db.getResourcesByTopicsForUser(id)
-  .then(data => {
-    const resource = {data:data};
-    res.render('index', { resource });
+  router.get("/signup", (req, res) => {
+    res.render("signup");
   })
-  .catch(err => {
-    console.error(err);
+
+  router.get("/:user_id", (req, res) => {
+    const id = req.params.user_id;
+    if (id) {
+      let userId = parseInt(req.params.user_id);
+      if (id === userId) {
+        db.getResourcesByTopicsForUser(id)
+          .then(data => {
+            const resource = { data: data };
+            res.render('index', { resource });
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      } else {
+        res.redirect("/")
+      }
+    } else {
+      res.redirect("/")
+    }
   });
-  // res.render('index')
-});
-return router;
+
+  router.post('/like/resource_id/user_id', (req, res) => {
+    //want resource that user liekd to be inserted into likes table with user id and resource id
+
+  });
+
+  return router;
 };
 
-router.post('/like/resource_id/user_id', (req, res) => {
-  //want resource that user liekd to be inserted into likes table with user id and resource id
 
-});
---
+++ routes/interactions.js
@@ -0,0 +1,8 @@
+module.exports = (db) => {
+  router.post('/like/resource_id/user_id', (req, res) => {
+    //want resource that user liekd to be inserted into likes table with user id and resource id
+
+  });
+
+  return router;
+};
