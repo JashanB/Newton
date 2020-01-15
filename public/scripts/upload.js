80827628431e3c6cd9c0834d0c484003905423ce
Katherine <katherine.szelag@mail.utoronto.ca>
2020-01-15 23:05:41 +0000

    upload is working

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
   $upload.submit(function() {
     event.preventDefault();
     const title = $("input[name='title']").val()
-    const description = $("input[name='description']").val()
+    const description = $("#resource-description").val()
     const imageURL = $("input[name='imageURL']").val()
     const resourceURL = $("input[name='resourceURL']").val()
     const topic = $( "select#topic-select" ).val();
 
-    if (title.length === 0 || description.length === 0 || imageURL === 0 || resourceURL.length === 0 || (!topic) ) {
+    if (title.length === 0 || description.length === 0 || imageURL.length === 0 || resourceURL.length === 0 || (!topic) ) {
       alert('Please make sure you fill in all the fields!')
     } else {
       $.post("/upload", $upload.serialize(), data => {
-        window.location.href ="http://localhost:8080/myresource/" + data.user_id;
+        window.location.href ="http://localhost:8080/myresources/" + data.user_id;
       })
     }
   })
